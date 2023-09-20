e190c08cd4ad74b757047c89307cf13377ce2dfc
sec <sec@spsmb.cz>
2023-09-20 15:32:05 +0200

    saveJokes

--- src/main/java/cz/spsmb/secda1/jokes/jokes_fx/HelloController.java
+++ src/main/java/cz/spsmb/secda1/jokes/jokes_fx/HelloController.java
@@ -1,12 +1,23 @@
 package cz.spsmb.secda1.jokes.jokes_fx;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
@@ -17,6 +28,13 @@ public class HelloController implements Initializable {
     @FXML
     private ListView<String> jokesListView;
 
+    @FXML
+    private TextField searchTextFiled;
+
+    @FXML
+    private TextArea jokeText;
+
+
     JokeService jokeService;
 
     @FXML
@@ -24,12 +42,53 @@ public class HelloController implements Initializable {
         welcomeText.setText(jokeService.getRandomJoke().getText());
     }
 
+    @FXML
+    protected void save(ActionEvent event) throws IOException {
+       String text = jokeText.getText();
+        System.out.println(text);
+        Joke joke = new Joke(jokeService.getNextId(), text);
+        jokeService.saveJoke(joke);
+        switchToAnotherView(event, "hello-view.fxml");
+    }
+
+
+    @FXML
+    protected void switchView(ActionEvent event) throws IOException {
+        switchToAnotherView(event, "create-joke-view.fxml");
+    }
+
+
+    @FXML
+    public void onSearch(){
+        System.out.println("Key pressed");
+        String searchText = searchTextFiled.getText();
+        List<String> filteredItems = new ArrayList<>();
+        for (Joke joke: jokeService.getJokes()){
+            if (joke.getText().contains(searchText)){
+                filteredItems.add(joke.getText());
+            }
+        }
+        jokesListView.setItems(FXCollections.observableList(filteredItems));
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         jokeService = new JokeService();
         jokeService.loadJokesFromCSVFile();
 
-        jokesListView.setItems(FXCollections.observableList(jokeService.getJokes().stream().map(Joke::getText)
-                .collect(Collectors.toList())));
+        if (jokesListView != null) {
+            jokesListView.setItems(FXCollections.observableList(jokeService.getJokes().stream().map(Joke::getText)
+                    .collect(Collectors.toList())));
+        }
+    }
+
+    private void switchToAnotherView(ActionEvent event, String s) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(s));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/cz/spsmb/secda1/jokes/jokes_fx/JokeService.java
+++ src/main/java/cz/spsmb/secda1/jokes/jokes_fx/JokeService.java
@@ -1,5 +1,8 @@
 package cz.spsmb.secda1.jokes.jokes_fx;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
@@ -19,6 +22,10 @@ public class JokeService {
         return jokes;
     }
 
+    public int getNextId(){
+        return jokes.size()+1;
+    }
+
     public void loadJokesFromCSVFile() {
         Scanner sc = new Scanner(this.getClass().getResourceAsStream("/shortjokes.csv"));
         sc.useDelimiter("\n");
@@ -30,5 +37,19 @@ public class JokeService {
             jokes.add(new Joke(id, text));
         }
         sc.close();
+    }
+
+    public void saveData() throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("R:\\verejne_vsichni\\PRO\\jokes_FX\\src\\main\\resources\\shortjokes.csv"));
+        for (Joke joke: jokes) {
+            bufferedWriter.write(joke.getId() + "," + '"' + joke.getText() + '"' + "\n");
+        }
+        bufferedWriter.close();
+    }
+
+    public void saveJoke(Joke joke) throws IOException {
+        jokes.add(joke);
+        saveData();
+
     }
 }
